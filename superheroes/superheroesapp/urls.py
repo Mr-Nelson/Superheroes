6f51b9b878fbadc3595976cc37abeded94108a60
Mr-Nelson <kdnels88@gmail.com>
2021-06-29 13:14:38 -0500

    created delete function, added delete prompt to index

--- superheroes/superheroesapp/urls.py
+++ superheroes/superheroesapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'superheroesapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:superhero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create_new_superhero')
+    path('<int:superhero_id>', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero'),
+    path('<int:superhero_id>', views.change, name='change'),
+    path('<int:superhero_id>', views.delete, name='delete')
 ]
--- superheroes/superheroesapp/views.py
+++ superheroes/superheroesapp/views.py
@@ -19,6 +19,23 @@ def detail(request, superhero_id):
     }
     return render(request, 'superheroesapp/detail.html', context)
 
+def change(request, superhero_id):
+    specific_superhero = Superhero.objects.filter(pk=superhero_id)
+    context = {
+        'specific_superhero': specific_superhero
+    }
+    if request.method == 'POST':
+        change_name = request.POST.get('change_name')
+        change_alter_ego = request.POST.get('change_alter_ego')
+        change_primary_ability = request.POST.get('change_primary_ability')
+        change_secondary_ability = request.POST.get('change_secondary_ability')
+        change_catchphrase = request.POST.get('change_catchphrase')
+        superhero_id = Superhero(name=change_name, alter_ego=change_alter_ego, primary_ability=change_primary_ability, secondary_ability=change_secondary_ability, catchphrase=change_catchphrase)
+        superhero_id.save()
+        return HttpResponseRedirect(reverse('superheroesapp:detail'))
+    else:
+        return render(request, 'superheroes/change.html', context)
+
 def create(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -28,6 +45,10 @@ def create(request):
         catchphrase = request.POST.get('catchphrase')
         new_superhero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
         new_superhero.save()
-        return HttpResponseRedirect(reverse('superherosapp:index'))
+        return HttpResponseRedirect(reverse('superheroesapp:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def delete(superhero_id):
+    superhero_id.delete()
+    return HttpResponseRedirect(reverse('superheroesapp:index'))
